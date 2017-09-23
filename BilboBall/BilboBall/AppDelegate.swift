a3ae10f1041e7631b1658a5349380e33969bb948
z3thon <rosson.long@lzteknology.com>
2017-09-23 13:10:32 -0600

    Starting Out
    The Beginning

---
+++ BilboBall/BilboBall/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  BilboBall
+//
+//  Created by Rosson Long on 9/23/17.
+//  Copyright © 2017 lztek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
